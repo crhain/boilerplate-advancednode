3fdccc3a8e3be98e25d62a80ab38fda389f19446
Carl Hain <crhain@gmail.com>
2017-05-23 13:47:58 -0500

    Set up project for heroku

--- server.js
+++ server.js
@@ -5,17 +5,31 @@ const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+const session = require('express-session');
+const passport = require('passport');
+const mongo = require('mongodb');
+
+app.set('view engine', 'pug');
 
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUnitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.route('/')
   .get((req, res) => {
-    res.sendFile(process.cwd() + '/views/index.html');
+    //res.sendFile(process.cwd() + '/views/index.html');
+    res.render('pug/index.pug', {title: 'Hello', message: "Please login"});
   });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Listening on port " + process.env.PORT);
+app.listen(PORT, () => {
+  console.log("Listening on port " + PORT);
 });
