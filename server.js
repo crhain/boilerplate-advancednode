647c6a34130a4e8a012f4abb606a5621fd9d3f83
Carl Hain <crhain@gmail.com>
2017-05-23 15:27:26 -0500

    Add logout functionality

--- server.js
+++ server.js
@@ -28,13 +28,25 @@ app.use(passport.session());
 app.route('/')
   .get((req, res) => {
     //res.sendFile(process.cwd() + '/views/index.html');
-    res.render('pug/index.pug', {title: 'Hello', message: "Please login"});
+    res.render('pug/index.pug', {title: 'Hello', message: "Please login", showLogin: true});
   });
 
 app.route('/profile')
   .get(ensureAuthenticated, (req,res) => {
-    res.render('pug/profile');                  
+    res.render('pug/profile', {username: req.user.username});                  
   });  
+
+app.route('/logout')
+  .get((req, res) => {
+      req.logout();
+      res.redirect('/');
+  });
+
+app.use((req, res, next) => {
+  res.status(404)
+    .type('text')
+    .send('Not Found');
+});  
 
 mongo.connect(process.env.DATABASE, (err, db) => {
     if(err) {
